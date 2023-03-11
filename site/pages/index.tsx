559558fb341122e2f9fcab281e0f45ab7329faba
DH <daniel@vendettistudios.com>
2023-03-11 01:54:48 -0800

    testing

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,6 +1,5 @@
 import Layout from '@components/common/Layout/Layout';
 import Carousel from '@components/ui/Carousel/Carousel';
-import Image from 'next/image';
 
 const Home = () => {
   const images = ['/Image1.jpeg', '/Image2.jpeg', '/Image3.jpeg'];

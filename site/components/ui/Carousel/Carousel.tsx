2bd55e0645e3812764eedc178bf724b8aa7b9a4e
DH <daniel@vendettistudios.com>
2023-03-11 01:08:55 -0800

    testing

--- site/components/ui/Carousel/Carousel.tsx
+++ site/components/ui/Carousel/Carousel.tsx
@@ -3,33 +3,33 @@ import Image from 'next/image';
 import s from './carousel.module.css';
 
 type CarouselProps = {
-  images: string[];
+    images: string[];
 };
 const Carousel = ({ images }: CarouselProps) => {
     const [activeIndex, setActiveIndex] = useState(0);
-  
+
     useEffect(() => {
-      const interval = setInterval(() => {
-        setActiveIndex((activeIndex + 1) % images.length);
-      }, 5000);
-      return () => clearInterval(interval);
+        const interval = setInterval(() => {
+            setActiveIndex((activeIndex + 1) % images.length);
+        }, 5000);
+        return () => clearInterval(interval);
     }, [activeIndex, images]);
-  
+
     return (
-      <div className={s.carouselContainer}>
-        <div className={s.imageCarousel}>
-          <div className={s.imageContainer}>
-            <Image
-              src={images[activeIndex]}
-              alt={`Image ${activeIndex}`}
-              width={400}
-              height={400}
-              style={{ objectFit: 'contain' }}
-              className={s.image}
-            />
-          </div>
+        <div className={s.carouselContainer}>
+            <div className={s.imageCarousel}>
+                <div className={s.imageContainer}>
+                    <Image
+                        src="/image1.jpeg"
+                        alt="Image 1"
+                        width={400}
+                        height={400}
+                        style={{ objectFit: 'contain' }}
+                        className={s.image}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
     );
-  };
+};
 export default Carousel;
